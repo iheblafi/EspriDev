331f7d767a44241d565dc6400792e545d53aae1b
Iheb Lafi <98402917+iheblafi@users.noreply.github.com>
2022-02-15 12:30:59 +0000

    Update PersonneCRUD.java

--- src/edu/services/PersonneCRUD.java
+++ src/edu/services/PersonneCRUD.java
@@ -17,10 +17,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author karra
- */
+
 public class PersonneCRUD {
     
     Connection cnx2;
